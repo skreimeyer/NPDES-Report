f6f8e3c0fccba611151963230076bd6847412088
skreimeyer <samuel.kreimeyer@gmail.com>
2019-03-17 16:25:35 -0500

    markdown generation script. Additions to template file

--- scripts/generate.py
+++ scripts/generate.py
@@ -2,5 +2,15 @@
 
 # Generate a markdown report
 
-import jinja2
+from jinja2 import Template
 import yaml
+
+# load reporting data
+with open('../data/report-data.yml','r') as infile:
+    report_data = yaml.load(infile)
+# load template
+with open('../templates/report.tmpl','r') as infile:
+    template = Template(infile.read())
+# render and write to report.md in the top folder
+with open('../report.md','w') as outfile:
+    outfile.write(template.render(report_data))
