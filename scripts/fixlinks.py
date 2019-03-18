4779c6e2a49d5af3ded2bff5dfa51fdc4aa23d5c
skreimeyer <samuel.kreimeyer@gmail.com>
2019-03-17 22:25:37 -0500

    formatting

--- scripts/fixlinks.py
+++ scripts/fixlinks.py
@@ -4,30 +4,30 @@ Throwaway script to format working links in the template.
 """
 import re
 
-with open('../templates/report.tmpl','r') as infile:
+with open("../templates/report.tmpl", "r") as infile:
     report = infile.readlines()
 
-link_preamble = re.compile(r'^\[')
-link_content = re.compile(r'(?<=\(#).*(?=\)$)')
-title_preamble = re.compile(r'^#+')
-title = re.compile(r'(?<=#)\s.*')
+link_preamble = re.compile(r"^\[")
+link_content = re.compile(r"(?<=\(#).*(?=\)$)")
+title_preamble = re.compile(r"^#+")
+title = re.compile(r"(?<=#)\s.*")
 
 
 # slug-ify link text in the table of contents
-for i,line in enumerate(report):
+for i, line in enumerate(report):
     if link_preamble.match(line):
         found = link_content.search(line)
         if found:
             link_string = found.group(0)
-            new_string = re.sub('\s','-',link_string.strip())
-            report[i] = re.sub(link_content,new_string,line)
+            new_string = re.sub("\s", "-", link_string.strip())
+            report[i] = re.sub(link_content, new_string, line)
     # make anchors
-    if title_preamble.match(line) and i >9:
+    if title_preamble.match(line) and i > 9:
         found = title.search(line)
         if found:
-            name_field = re.sub('\s','-',found.group(0).strip())
+            name_field = re.sub("\s", "-", found.group(0).strip())
             anchor = f'<a name="{name_field}"></a>\n'
-            report[i] = line.strip()+anchor
+            report[i] = line.strip() + anchor
 
-with open('../templates/report-fix.tmpl','w') as outfile:
+with open("../templates/report-fix.tmpl", "w") as outfile:
     outfile.writelines(report)
--- scripts/generate.py
+++ scripts/generate.py
@@ -6,11 +6,11 @@ from jinja2 import Template
 import yaml
 
 # load reporting data
-with open('../data/report-data.yml','r') as infile:
+with open("../data/report-data.yml", "r") as infile:
     report_data = yaml.load(infile)
 # load template
-with open('../templates/report.tmpl','r') as infile:
+with open("../templates/report.tmpl", "r") as infile:
     template = Template(infile.read())
 # render and write to report.md in the top folder
-with open('../report.md','w') as outfile:
+with open("../report.md", "w") as outfile:
     outfile.write(template.render(report_data))
