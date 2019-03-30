f284d525215330003dbec8a17e8a646d80e6d3c3
skreimeyer <samuel.kreimeyer@gmail.com>
2019-03-30 09:52:10 -0500

    modification to chart generator

--- scripts/generate_seasonal.py
+++ scripts/generate_seasonal.py
@@ -11,15 +11,35 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("../data/CLR DMR.csv")
 
+params = df['Parameter'].unique()
 # cast 'Date' column as dates
 df['Date'] = pd.to_datetime(df['Date'])
-# Get our seasons
-# winter = df.loc[df['Date'].dt.month==2]
-# spring = df.loc[df['Date'].dt.month==5]
-# summer = df.loc[df['Date'].dt.month==8]
-# fall = df.loc[df['Date'].dt.month==11]
+#make a 'Seasons' column
+seasonmap = {2:'Winter',5:'Spring',8:'Summer',11:'Fall'}
+df['Season'] = df['Date'].dt.month # intermediate step
+df['Season'] = df['Season'].apply(lambda x: seasonmap[x])
 
 # Make these new fields instead and work on main dataframe
+# Main loop
+for p in params:
+    localframe = df.loc[df['Parameter']==p]
+    unit = localframe.iloc[1]['Unit']
+    # make seasonal graph
+    ax = localframe.boxplot(column='Value',by='Season')
+    ax.figure.savefig(f'../figures/seasonal-{p}.png')
+    plt.cla()
+    plt.clf()
+    # make location graph
+    ax = localframe.boxplot(column='Value',by='Location')
+    ax.figure.savefig(f'../figures/seasonal-{p}.png')
+    plt.cla()
+    plt.clf()
+    fig1,ax1 = plt.subplots(figsize=(6,4),dpi=300)
+    ax1.set_title(f'Time-Series Analysis of {p} Within MS4',size=24)
+    ax1.set_xlabel(f'{units})
+
+
+
 
 # What does work:
 # df.boxplot(column='Value',by='Location') -> ax.figure.savefig()
